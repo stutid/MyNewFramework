d377977f591bc0db5cf3e0fd4f25ed6b87f68538
Stuti <stuti@Stutis-MacBook-Pro.local>
2018-09-16 20:06:59 +0530

    Again pushing framework files

---
+++ MyNewFramework/MyNewFramework/MyNewFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyNewFramework.h
+//  MyNewFramework
+//
+//  Created by Stuti on 16/09/18.
+//  Copyright © 2018 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyNewFramework.
+FOUNDATION_EXPORT double MyNewFrameworkVersionNumber;
+
+//! Project version string for MyNewFramework.
+FOUNDATION_EXPORT const unsigned char MyNewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyNewFramework/PublicHeader.h>
+
+
---
+++ MyNewFramework/MyNewFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  MyNewFramework
+//
+//  Created by Stuti on 16/09/18.
+//  Copyright © 2018 iOS. All rights reserved.
+//
+
+import Foundation
+
+class Service {
+    
+    private init() {}
+    
+    public static func newFunction() -> String {
+        return "This is my newest framework"
+    }
+}
